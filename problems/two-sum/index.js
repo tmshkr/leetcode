04d996f07c403c697845113d620c9b70759d60aa
Tim Shaker <tim@tmshkr.com>
2022-05-16 16:54:26 -0700

    add solution

--- problems/two-sum/index.js
+++ problems/two-sum/index.js
@@ -3,8 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    
+var twoSum = function (nums, target) {
+  const hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (complement in hash) {
+      return [hash[complement], i];
+    }
+    hash[nums[i]] = i;
+  }
 };
 
 module.exports = { twoSum };
--- problems/two-sum/test.js
+++ problems/two-sum/test.js
@@ -1,14 +1,14 @@
 const { twoSum } = require("./index.js");
 
-test("[2,7,11,15]", () => {
-  expect(twoSum([2,7,11,15])).toEqual(9);
+test("[2,7,11,15],9", () => {
+  expect(twoSum([2,7,11,15],9)).toEqual([0,1]);
 });
 
-test("[3,2,4]", () => {
-  expect(twoSum([3,2,4])).toEqual(6);
+test("[3,2,4],6", () => {
+  expect(twoSum([3,2,4],6)).toEqual([1,2]);
 });
 
-test("[3,3]", () => {
-  expect(twoSum([3,3])).toEqual(6);
+test("[3,3],6", () => {
+  expect(twoSum([3,3],6)).toEqual([0,1]);
 });
 
