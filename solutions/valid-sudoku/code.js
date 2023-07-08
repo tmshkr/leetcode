4b694be2a3c158be6b5336696d1620efcc3e5b7d
Tim Shaker <tim@tmshkr.com>
2023-07-08 11:05:09 -0700

    36. Valid Sudoku

--- solutions/valid-sudoku/code.js
+++ solutions/valid-sudoku/code.js
@@ -2,8 +2,27 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function(board) {
-    
+var isValidSudoku = function (board) {
+  const rows = new Array(9).fill(0).map(() => new Set());
+  const cols = new Array(9).fill(0).map(() => new Set());
+  const boxes = new Array(9).fill(0).map(() => new Set());
+
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      const value = board[i][j];
+      if (value === ".") continue;
+
+      const b = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+      if (rows[i].has(value) || cols[j].has(value) || boxes[b].has(value)) {
+        return false;
+      }
+
+      rows[i].add(value);
+      cols[j].add(value);
+      boxes[b].add(value);
+    }
+  }
+  return true;
 };
 
 module.exports = { isValidSudoku };
--- solutions/valid-sudoku/code.py
+++ solutions/valid-sudoku/code.py
@@ -4,6 +4,25 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
+        cols = [set() for _ in range(9)]
+        rows = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+                if num == '.':
+                    continue
+
+                box = (i // 3) * 3 + j // 3
+                if num in cols[i] or num in rows[j] or num in boxes[box]:
+                    return False
+
+                cols[i].add(num)
+                rows[j].add(num)
+                boxes[box].add(num)
+
+        return True
         
 
 # https://leetcode.com/problems/valid-sudoku/
