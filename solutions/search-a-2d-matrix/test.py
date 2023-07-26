b1a3d6dd2c796341347a90b0dc8a1ba3a8a0f2dd
Tim Shaker <tim@tmshkr.com>
2023-07-26 10:09:15 -0700

    74. Search a 2D Matrix

---
+++ solutions/search-a-2d-matrix/test.py
@@ -0,0 +1,16 @@
+import unittest
+from code import Solution
+
+s = Solution()
+
+
+class TestSolution(unittest.TestCase):
+	def test_0(self):
+		self.assertEqual(s.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],3), True)
+
+	def test_1(self):
+		self.assertEqual(s.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],13), False)
+
+
+if __name__ == "__main__":
+	unittest.main()
