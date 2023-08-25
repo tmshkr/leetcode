cd31b457dd67bf745a682e20db898fca1afa8bf3
Tim Shaker <tim@tmshkr.com>
2023-08-25 10:40:44 -0700

    btree utils

---
+++ src/utils/binary-tree.js
@@ -0,0 +1,38 @@
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+function createBinaryTree(levelOrderList) {
+  if (!levelOrderList) {
+    return null;
+  }
+
+  const root = new TreeNode(levelOrderList[0]);
+  const q = [root];
+  let index = 1;
+
+  while (q.length && index < levelOrderList.length) {
+    const node = q.shift();
+
+    const leftVal = levelOrderList[index];
+    if (leftVal !== null) {
+      node.left = new TreeNode(leftVal);
+      q.push(node.left);
+    }
+    index++;
+
+    if (index < levelOrderList.length) {
+      const rightVal = levelOrderList[index];
+      if (rightVal !== null) {
+        node.right = new TreeNode(rightVal);
+        q.push(node.right);
+      }
+      index++;
+    }
+  }
+  return root;
+}
+
+module.exports = { createBinaryTree, TreeNode };
