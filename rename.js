17a6dfdba72b784d4e2ccce5c35f6455817f175a
Tim Shaker <tim@tmshkr.com>
2023-09-16 09:52:25 -0700

    update script

--- rename.js
+++ rename.js
@@ -1,20 +1,23 @@
 const fs = require("fs");
-const path = require("path");
 const { glob } = require("glob");
 
 async function rename() {
   const files = await glob("**/code.*", { ignore: "**/__pycache__/**" });
 
   files.forEach((file) => {
-    const oldFilePath = file;
-    const [fileName, extension] = oldFilePath.split(".");
+    const [fileName, extension] = file.split(".");
     const newFilePath = fileName.replace("code", "solution") + "." + extension;
+    const testFilePath = fileName.replace("code", "test") + "." + extension;
 
     try {
-      fs.renameSync(oldFilePath, newFilePath);
-      console.log(`Renamed: ${oldFilePath} -> ${newFilePath}`);
+      const updatedTest = fs
+        .readFileSync(testFilePath, "utf8")
+        .replace("code", "solution");
+      fs.writeFileSync(testFilePath, updatedTest, "utf8");
+      fs.renameSync(file, newFilePath);
+      console.log(`Renamed: ${file} -> ${newFilePath}`);
     } catch (error) {
-      console.error(`Error renaming ${oldFilePath}: ${error.message}`);
+      console.error(`Error renaming ${file}: ${error.message}`);
     }
   });
 }
