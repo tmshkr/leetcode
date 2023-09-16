9c98d095a0de2d3f2765ac19e91f7ce4656777c0
Tim Shaker <tim@tmshkr.com>
2023-09-15 22:34:16 -0700

    rename.js

---
+++ rename.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+const { glob } = require("glob");
+
+async function rename() {
+  const files = await glob("**/code.*", { ignore: "**/__pycache__/**" });
+
+  files.forEach((file) => {
+    const oldFilePath = file;
+    const [fileName, extension] = oldFilePath.split(".");
+    const newFilePath = path.join(
+      fileName.replace("code", "solution") + "." + extension
+    );
+
+    try {
+      fs.renameSync(oldFilePath, newFilePath);
+      console.log(`Renamed: ${oldFilePath} -> ${newFilePath}`);
+    } catch (error) {
+      console.error(`Error renaming ${oldFilePath}: ${error.message}`);
+    }
+  });
+}
+
+rename();
