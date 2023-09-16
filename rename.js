45b9e3dd4eb50e23c50221520b9dd7b33cd7f1a1
Tim Shaker <tim@tmshkr.com>
2023-09-15 22:38:20 -0700

    update script

--- rename.js
+++ rename.js
@@ -8,9 +8,7 @@ async function rename() {
   files.forEach((file) => {
     const oldFilePath = file;
     const [fileName, extension] = oldFilePath.split(".");
-    const newFilePath = path.join(
-      fileName.replace("code", "solution") + "." + extension
-    );
+    const newFilePath = fileName.replace("code", "solution") + "." + extension;
 
     try {
       fs.renameSync(oldFilePath, newFilePath);
