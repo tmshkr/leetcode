e40e3a9524325bff6af321f9bafbf4c62850a0b6
Tim Shaker <tim@tmshkr.com>
2023-11-10 11:45:26 -0800

    rename var

--- solutions/detect-squares/solution.js
+++ solutions/detect-squares/solution.js
@@ -9,13 +9,13 @@ class DetectSquares {
     this.counter.add([x, y]);
   }
 
-  count([x, y]) {
+  count([px, py]) {
     let count = 0;
 
-    for (const [x2, y2] of this.points) {
-      if (x2 === x || y2 === y) continue;
-      if (Math.abs(x2 - x) !== Math.abs(y2 - y)) continue;
-      count += this.counter.count([x, y2]) * this.counter.count([x2, y]);
+    for (const [x, y] of this.points) {
+      if (x === px || y === py) continue;
+      if (Math.abs(x - px) !== Math.abs(y - py)) continue;
+      count += this.counter.count([px, y]) * this.counter.count([x, py]);
     }
 
     return count;
