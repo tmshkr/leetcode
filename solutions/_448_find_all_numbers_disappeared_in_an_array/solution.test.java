79a6e4ba5ccf0259035ebfa1f9f5b36b3cb6396f
Tim Shaker <tim@tmshkr.com>
2023-12-02 10:44:44 -0800

    448. Find All Numbers Disappeared in an Array

--- solutions/_448_find_all_numbers_disappeared_in_an_array/solution.test.java
+++ solutions/_448_find_all_numbers_disappeared_in_an_array/solution.test.java
@@ -1,28 +1,37 @@
-
 package _448_find_all_numbers_disappeared_in_an_array;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class SolutionTest {
 
-  
-    @Test
-    @DisplayName("[4,3,2,7,8,2,3,1]")
-    void findDisappearedNumbers0() {
-      Solution s = new Solution();
-      // inputs = [4,3,2,7,8,2,3,1]
-      // expected = [5,6]
-    }
-    
-    @Test
-    @DisplayName("[1,1]")
-    void findDisappearedNumbers1() {
-      Solution s = new Solution();
-      // inputs = [1,1]
-      // expected = [2]
-    }
-    
+  @Test
+  @DisplayName("[4,3,2,7,8,2,3,1]")
+  void findDisappearedNumbers0() {
+    Solution s = new Solution();
+    int[] inputs = { 4, 3, 2, 7, 8, 2, 3, 1 };
+    int[] expected = { 5, 6 };
+    int[] actual = s
+      .findDisappearedNumbers(inputs)
+      .stream()
+      .mapToInt(i -> i)
+      .toArray();
+    assertArrayEquals(expected, actual);
+  }
+
+  @Test
+  @DisplayName("[1,1]")
+  void findDisappearedNumbers1() {
+    Solution s = new Solution();
+    int[] inputs = { 1, 1 };
+    int[] expected = { 2 };
+    int[] actual = s
+      .findDisappearedNumbers(inputs)
+      .stream()
+      .mapToInt(i -> i)
+      .toArray();
+    assertArrayEquals(expected, actual);
+  }
 }
