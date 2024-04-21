36dd11bb1fc342372e084d0d36f5503ace97ff90
Tim Shaker <tim@tmshkr.com>
2024-04-21 11:14:35 -0700

    remove comments

--- solutions/_951_flip_equivalent_binary_trees/Solution.test.java
+++ solutions/_951_flip_equivalent_binary_trees/Solution.test.java
@@ -12,9 +12,6 @@ class SolutionTest {
   @DisplayName("[1,2,3,4,5,6,null,null,null,7,8],[1,3,2,null,6,4,5,null,null,null,null,8,7]")
   void flipEquiv0() {
     Solution s = new Solution();
-    // inputs =
-    // [1,2,3,4,5,6,null,null,null,7,8],[1,3,2,null,6,4,5,null,null,null,null,8,7]
-    // expected = true
     TreeNode root1 = TestUtils.createBinaryTree(new Integer[] { 1, 2, 3, 4, 5, 6, null, null, null, 7, 8 });
     TreeNode root2 = TestUtils.createBinaryTree(new Integer[] { 1, 3, 2, null, 6, 4, 5, null, null, null, null, 8, 7 });
     boolean expected = true;
@@ -26,8 +23,6 @@ class SolutionTest {
   @DisplayName("[],[]")
   void flipEquiv1() {
     Solution s = new Solution();
-    // inputs = [],[]
-    // expected = true
     TreeNode root1 = TestUtils.createBinaryTree(new Integer[] {});
     TreeNode root2 = TestUtils.createBinaryTree(new Integer[] {});
     boolean expected = true;
@@ -39,8 +34,6 @@ class SolutionTest {
   @DisplayName("[],[1]")
   void flipEquiv2() {
     Solution s = new Solution();
-    // inputs = [],[1]
-    // expected = false
     TreeNode root1 = TestUtils.createBinaryTree(new Integer[] {});
     TreeNode root2 = TestUtils.createBinaryTree(new Integer[] { 1 });
     boolean expected = false;
