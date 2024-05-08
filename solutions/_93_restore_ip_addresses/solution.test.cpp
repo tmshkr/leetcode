1bf918c1bf11d6ce43b92fb7639aad23d2a9b25e
Tim Shaker <tim@tmshkr.com>
2024-05-08 09:08:12 -0700

    remove comments

--- solutions/_93_restore_ip_addresses/solution.test.cpp
+++ solutions/_93_restore_ip_addresses/solution.test.cpp
@@ -7,9 +7,6 @@ using namespace std;
 TEST(SolutionTest, Test1)
 {
   Solution s;
-  // inputs = "25525511135"
-  // expected = ["255.255.11.135","255.255.111.35"]
-
   vector<string> expected = {"255.255.11.135", "255.255.111.35"};
   vector<string> actual = s.restoreIpAddresses("25525511135");
   EXPECT_EQ(expected, actual);
@@ -18,9 +15,6 @@ TEST(SolutionTest, Test1)
 TEST(SolutionTest, Test2)
 {
   Solution s;
-  // inputs = "0000"
-  // expected = ["0.0.0.0"]
-
   vector<string> expected = {"0.0.0.0"};
   vector<string> actual = s.restoreIpAddresses("0000");
   EXPECT_EQ(expected, actual);
@@ -29,9 +23,6 @@ TEST(SolutionTest, Test2)
 TEST(SolutionTest, Test3)
 {
   Solution s;
-  // inputs = "101023"
-  // expected = ["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
-
   vector<string> expected = {"1.0.10.23", "1.0.102.3", "10.1.0.23", "10.10.2.3", "101.0.2.3"};
   vector<string> actual = s.restoreIpAddresses("101023");
   EXPECT_EQ(expected, actual);
