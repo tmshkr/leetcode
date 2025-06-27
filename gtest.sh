defb1b9e5d9c2349c93eea35d9582be09b3fcac6
tmshkr <tim@tmshkr.com>
2025-06-27 10:46:36 -0700

    update gtest.sh

--- gtest.sh
+++ gtest.sh
@@ -2,22 +2,23 @@
 
 # Compiles and runs Google Test on a specified directory of C++ files.
 
+build_dir="$(pwd)/build"
+
 # Navigate to the specified directory
 cd $1
 
 # Remove the existing build directory
-echo "Removing $1/build"
-rm -rf build
+echo "Removing $build_dir"
+rm -rf "$build_dir"
 
 # Create a new build directory
-echo "Creating $1/build"
-cmake -S . -B build
+echo "Creating $build_dir"
+cmake -S . -B "$build_dir"
 
 # Build the project
-echo "Building $1/build"
-cmake --build build
+echo "Building $build_dir"
+cmake --build "$build_dir"
 
 # Run the tests
-echo "Running $1/build/test"
-cd build && ctest --rerun-failed --output-on-failure
-
+echo "Running $build_dir/test"
+cd "$build_dir" && ctest --rerun-failed --output-on-failure
